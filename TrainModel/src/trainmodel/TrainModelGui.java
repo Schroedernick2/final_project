03a9adba9e370fbb26ef479f6de86f005680fdf3
nicky <Schroedernick2@aol.com>
2018-12-11 17:13:00 -0500

    prepping for station stopping

--- TrainModel/src/trainmodel/TrainModelGui.java
+++ TrainModel/src/trainmodel/TrainModelGui.java
@@ -741,7 +741,7 @@ public class TrainModelGui extends javax.swing.JDialog {
                         eElement.setAttribute("distanceTraveled",""+t.getDistance());
                         eElement.setAttribute("next",""+t.getNext());
                         
-                        if(t.stops.get(t.numberOfStops).contains(nextStation)){
+                        if(t.stops.get(t.numberOfStops).equals(nextStation)){
                             t.numberOfStops++;
                         }
                     }
